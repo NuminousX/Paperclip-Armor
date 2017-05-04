cf89fb7304e62feef012561817aac6ef7945b27d
NuminousX <yarithjoseph@gmail.com>
2017-05-05 00:21:34 +0200

    should work now

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+$('.BG').mousemove(function( event ) {
+    
+var containerWidth = $(this).innerWidth(),
+    containerHeight = $(this).innerHeight(),
+    mousePositionX = (event.pageX / containerWidth) * 100,
+    mousePositionY = (event.pageY /containerHeight) * 100;
+
+$(this).css('background-position', mousePositionX + '%' + ' ' + mousePositionY + '%');
+
+});
